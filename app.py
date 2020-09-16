8f53fee6f55bebe067cc75a3293d6c5b99c63181
sallyfelixia <70709103+sallyfelixia@users.noreply.github.com>
2020-09-16 21:32:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def next_available_row(sheet):
     try:
         str_list = list(filter(None, sheet.col_values(1)))
         return len(str_list)+1
-   except:
+    except:
         return 0
 
 # 處理訊息
