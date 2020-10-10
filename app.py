19cd6ad2c9cc8a10e5aa0f80c8c5845afdfdcc5b
sallyfelixia <70709103+sallyfelixia@users.noreply.github.com>
2020-10-10 20:13:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -107,11 +107,10 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, message)
     elif 'check cost info' in msg:
-        messsage = ''
+        message = ''
         for i in range(1,7,1):
             message += sheet_cost.cell(i,7)
             message += sheet_cost.cell(i,8)
-            message += '\n'
         line_bot_api.reply_message(event.reply_token, message)
     elif 'off' in msg:
         message = TextSendMessage(text= 'light is turned off')
