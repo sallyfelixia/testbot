f2154d7c0f59b9301d3a7c6f877805d5a28a42bc
sallyfelixia <70709103+sallyfelixia@users.noreply.github.com>
2020-10-10 18:06:34 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,213 +1,42 @@
-_row = 2
-
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+#這些是LINE官方開放的套件組合透過import來套用這個檔案上
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-import traceback
-#======python的函數庫==========
-
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-
-
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json",scope)
-
-client = gspread.authorize(creds)
-sheet = client.open("test_1_db")
-sheet_loc = sheet.get_worksheet(0)
-sheet_cost = sheet.get_worksheet(1)
-sheet_wal = sheet.get_worksheet(2)
-sheet_light = sheet.get_worksheet(3)
-sheet_pass = sheet.get_worksheet(4)
-
-
-def static_vars(**kwargs):
-    def decorate(func):
-        for k in kwargs:
-            setattr(func, k, kwargs[k])
-        return func
-    return decorate
-
-
-
-
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-
-# Channel Access Token
-line_bot_api = LineBotApi('MBzS1ky44RKM/kfRt70FjKCXIcjdPtbPTirlnrBcllV3dlJL+j5cHCk/rfEWHM/Mnnfx2aak3NcWJzSVk/AQ9Fh2wu+YrldRU7uMowmsTbLoMzmYqLx4mLb5iXNSxY8fE5el4n071I8LFQCOI17e8AdB04t89/1O/w1cDnyilFU=')
-
-# Channel Secret
-handler = WebhookHandler('8d52e9a3b33bafa81896c30ee33144ad')
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-@static_vars(counter=0)
-def handle_message(event):
-    
-    num_loc = sheet_loc.cell(2,7).value
-    num_cost = sheet_cost.cell(2,5).value
-    msg = event.message.text
-    user_id = event.source.user_id
-    
-    if 'hello' in msg:
-        message = TextSendMessage(text= 'hello")
-        line_bot_api.reply_message(event.reply_token, message)
-    
-    '''
-    if 'check last ID-CARD location' in msg:
-        row = int(sheet_loc.cell(2,7).value) + 1 
-        sim_lon = int(sheet_loc.cell(row,2).value) + 0.00000000000001 * int(sheet_loc.cell(row,3).value)
-        sim_lat = int(sheet_loc.cell(row,4).value) + 0.00000000000001 * int(sheet_loc.cell(row,5).value)
-        message = LocationSendMessage(
-            title='previous location',
-            address='上一次拿出卡片的位置',
-            latitude = sim_lat,
-            longitude = sim_lon
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-    if 'check present wallet location' in msg:
-        sim_lon = int(sheet_wal.cell(2,1).value) + 0.00000000000001 * int(sheet_wal.cell(2,2).value)
-        sim_lat = int(sheet_wal.cell(2,3).value) + 0.00000000000001 * int(sheet_wal.cell(2,4).value)
-        message = LocationSendMessage(
-            title='walltet location',
-            address='錢包的現在位置',
-            latitude = sim_lat,
-            longitude = sim_lon
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-    if 'check cost info' in msg:
-        cost = [0, 0, 0, 0, 0, 0]
-        for i in range (2,int(sheet_loc.cell(2,7).value)+3,1)
-            for k in range 6:
-                if int(sheet_loc.cell(i,2).value == k:
-                    cost[k-1] += sheet_loc.cell(i,3).value
-        message =  ('Food : ' + str(cost[0]) + '\n' + 
-                    'Clothing : ' + str(cost[1]) + '\n' + 
-                    'Housing : ' + str(cost[2]) + '\n' + 
-                    'Transportation : ' + str(cost[3]) + '\n' +
-                    'Education : ' + str(cost[4]) + '\n' +
-                    'Entertainment : ' + str(cost[5]))
-        line_bot_api.reply_message(event.reply_token, message)
-    if 'set wallet password' in msg:
-        message = TextSendMessage(text= 'please enter your "auth" + "new password")
-        line_bot_api.reply_message(event.reply_token, message)
-    if 'auth' in msg:
-        digit = 3
-        password = 0
-        for i in range (4,8,1):
-            password += msg[i]*(10**digit)
-            digit -= 1
-        sheet_pass.update_cell(1, 1, str(password))
-    if 'turn on signal light' in msg:
-        message = TextSendMessage(text= 'enter "off" to turn off")
-        line_bot_api.reply_message(event.reply_token, message)
-        sheet_light.update_cell(1, 1, str(1))
-    if 'off' in msg:
-        message = TextSendMessage(text= 'light is turned off")
-        line_bot_api.reply_message(event.reply_token, message)
-        sheet_light.update_cell(1, 1, str(0))
-    else:
-        message = TemplateSendMessage(
-            alt_text='Carousel menu',
-            template=CarouselTemplate(
-                columns=[
-                    CarouselColumn(
-                        thumbnail_image_url='https://imgur.com/3whWd6A.png',
-                        title='ID-card location',
-                        text='check last ID-CARD location',
-                        actions=[
-                            MessageTemplateAction(
-                                label='tap to check',
-                                text='check last ID-CARD location'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://imgur.com/Ev4ToWr.png',
-                        title='Wallet location',
-                        text='check present wallet location',
-                        actions=[
-                            MessageTemplateAction(
-                                label='tap to check',
-                                text='check present wallet location'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://imgur.com/5NqGKmh.png',
-                        title='Cost',
-                        text='check cost info',
-                        actions=[
-                            MessageTemplateAction(
-                                label='tap to check',
-                                text='check cost info'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://imgur.com/oczX1yI.png',
-                        title='Light',
-                        text='turn on signal light',
-                        actions=[
-                            MessageTemplateAction(
-                                label='tap to light up',
-                                text='turn on signal light'
-                            )
-                        ]
-                    ),
-                    CarouselColumn(
-                        thumbnail_image_url='https://imgur.com/McGA5nL.png',
-                        title='Password setting',
-                        text='set wallet password',
-                        actions=[
-                            MessageTemplateAction(
-                                label='tap to set',
-                                text='set wallet password'
-                            )
-                        ]
+def test():
+    message = TemplateSendMessage(
+        alt_text='圖片旋轉木馬',
+        template=ImageCarouselTemplate(
+            columns=[
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/uKYgfVs.jpg",
+                    action=URITemplateAction(
+                        label="新鮮水果",
+                        uri="http://img.juimg.com/tuku/yulantu/110709/222-110F91G31375.jpg"
                     )
-                ], image_aspect_ratio = 'rectangle', image_size = 'cover'
-            )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QOcAvjt.jpg",
+                    action=URITemplateAction(
+                        label="新鮮蔬菜",
+                        uri="https://cdn.101mediaimage.com/img/file/1410464751urhp5.jpg"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
+                    action=URITemplateAction(
+                        label="可愛狗狗",
+                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
+                    )
+                ),
+                ImageCarouselColumn(
+                    image_url="https://i.imgur.com/QRIa5Dz.jpg",
+                    action=URITemplateAction(
+                        label="可愛貓咪",
+                        uri="https://m-miya.net/wp-content/uploads/2014/07/0-065-1.min_.jpg"
+                    )
+                )
+            ]
         )
-    '''
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    )
+    return message
