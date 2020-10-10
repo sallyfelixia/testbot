ac493bae5bb29b5910823c82c6ae33be205e3abd
sallyfelixia <70709103+sallyfelixia@users.noreply.github.com>
2020-10-10 19:08:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     elif 'check cost info' in msg:
         cost = [0, 0, 0, 0, 0, 0]
-        for i in range (2,int(sheet_loc.cell(2,7).value)+3,1):
-            for k in range 6:
+        for i in range (2,int(sheet_cost.cell(2,7).value)+3,1):
+            for k in range(6):
                 if int((sheet_cost.cell(i,2).value) == k:
                     cost[k-1] += int(sheet_cost.cell(i,3).value)
         message =  ('Food : ' + str(cost[0]) + ' ' + 
