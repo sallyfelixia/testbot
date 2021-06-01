63d4f6df7fee0226302df09b82630016836419b9
sallyfelixia <70709103+sallyfelixia@users.noreply.github.com>
2021-06-01 11:38:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,6 +118,10 @@ def handle_message(event):
                     seatnum = sheet_name.cell(i,3).value
                     _class = sheet_name.cell(i,2).value
                     name_exist = True
+         if name_exist:
+            message = TextSendMessage(text= '你好'+name)
+            line_bot_api.reply_message(event.reply_token, message)
+                   
         '''
     elif log_in_state == 1:
         name = msg
